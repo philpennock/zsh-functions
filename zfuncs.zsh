8227248966722a36a91058a43a92ca9c0bbf34c6
Phil Pennock <phil+git@pennock-tech.com>
2020-05-12 18:11:11 -0400

    Pull in my new k-privilege-OS functions

--- zfuncs.zsh
+++ zfuncs.zsh
@@ -23,15 +23,30 @@ if have_cmd kubectl; then
     eval "function k-$T { local n=\"$T-\$RANDOM\"; echo >&2 \"pod: \$n\"; k run -it --rm --restart=Never --image='pennocktech/ci:$T' \$n -- \"\$@\" }"
   done; unset T
 
-  # These k-OS functions can take an optional :TAG first parameter, eg `k-ubuntu :focal`
+  # These k-OS functions can take an optional :TAG first parameter,
+  #   eg `k-ubuntu :focal`
   # Other parameters are passed through
+  #
+  # The k-privileged-OS functions are grotesquely powerful, but beware that the
+  # args manipulation into JSON will only work for sufficiently simple strings
+  # (because zsh doesn't have a "turn this array var into a JSON string
+  # representation" expansion flag, but I get part-way there).
   for I in alpine busybox debian ubuntu; do
     eval "function k-$I {
       local n tag;
       case \$1 in (:*) tag=\"\${1#:}\"; shift;; (*) tag=latest;; esac;
-      n=\"$I-\${tag//./-}-\$RANDOM\";
+      n=\"$LOGNAME-$I-\${tag//./-}-\$RANDOM\";
       echo >&2 \"pod: \$n\";
       k run -it --rm --restart=Never --image=\"$I:\$tag\" \$n -- \"\$@\"
+    }"
+    eval "function k-privileged-$I {
+      local n tag ov;
+      case \$1 in (:*) tag=\"\${1#:}\"; shift;; (*) tag=latest;; esac;
+      n=\"$LOGNAME-$I-\${tag//./-}-\$RANDOM\";
+      echo >&2 \"pod: \$n\";
+      ov=\"\$(jq --arg n \"\$n\" --arg i \"$I:\$tag\" --argjson args \"[\${(j:,:)\${(@qqq)@}}]\" -n '.spec.containers[0]={name:\$n,image:\$i,stdin:true,tty:true,securityContext:
+{privileged:true},args:\$args}|.spec.hostNetwork=true|.spec.hostPID=true')\"
+      k run -it --rm --restart=Never --image=\"$I:\$tag\" --overrides=\"\$ov\" \$n -- \"\$@\"
     }"
   done; unset I
 
